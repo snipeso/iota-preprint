963c834035f30aa4ecfbab9a2f2022a08851ba73
Sophia Snipes <snipeso@users.noreply.github.com>
2024-07-18 22:39:22 +0200

    little script to quickly run preprocessing

---
+++ HBN_Wake/untitled2.m
@@ -0,0 +1,23 @@
+close all
+clc
+figure
+plot([1 2], [1 2], 'Color', [0 1 0])
+axis off
+
+chART.save_figure('Test', cd, PlotProps)
+
+%%
+% Your plot code here
+saveas(gcf, 'myfigure.svg');
+
+% Read the SVG file
+svgFile = 'myfigure.svg';
+fileText = fileread(svgFile);
+
+% Replace color-interpolation from linearRGB to sRGB
+fileText = strrep(fileText, 'color-interpolation:linearRGB;', 'color-interpolation:sRGB;');
+
+% Write the modified content back to the SVG file
+fid = fopen(svgFile, 'w');
+fwrite(fid, fileText);
+fclose(fid);
