c856828e003da4fc3edbd904a9bba7968adcc61a
Sophia Snipes <snipeso@users.noreply.github.com>
2024-07-21 22:25:42 +0200

    checked examples

--- HBN_Wake/Figure4_Topography.m
+++ HBN_Wake/Figure4_Topography.m
@@ -1,4 +1,4 @@
-
+% Scripts to plot the topographies based on the individual iota peaks
 clear
 clc
 close all
@@ -16,14 +16,7 @@ end
 
 load(fullfile(CacheDir, CacheName),  'Metadata', 'CustomTopographies', 'Chanlocs')
 
-CustomTopographies = squeeze(CustomTopographies(:, end, :));
-Remove = ~any(isnan(CustomTopographies), 2);
-CleanTopo = CustomTopographies(Remove, :);
-ClusterMetadata = Metadata(~Remove, :);
-
-[~, Order] = sort(max(CleanTopo, [], 2), 'descend');
-CleanTopo = CleanTopo(Order, :);
-ClusterMetadata = ClusterMetadata(Order, :);
+CleanTopo = squeeze(CustomTopographies(:, end, :));
 
 PlotProps = Parameters.PlotProps.Manuscript;
 PlotProps.External.EEGLAB.TopoRes = 100;
@@ -32,10 +25,15 @@ PlotProps.External.EEGLAB.TopoRes = 100;
 figure('Units','normalized', 'OuterPosition',[0 0 1 1])
 IndexPlot = 1;
 for Index =1:size(CleanTopo, 1)
+
+    Data = CleanTopo(Index, :);
+    if any(isnan(Data))
+        continue
+    end
     subplot(8, 12, IndexPlot)
-    chART.plot.eeglab_topoplot(CleanTopo(Index, :), Chanlocs, [], [], '', 'Linear', PlotProps)
+    chART.plot.eeglab_topoplot(Data, Chanlocs, [], [], '', 'Linear', PlotProps)
     IndexPlot = IndexPlot+1;
-    title([ClusterMetadata.EID{Index}, ' (', num2str(round(ClusterMetadata.Age(Index))) 'yo)'], 'FontWeight','normal', 'FontSize', 8)
+    title([Metadata.EID{Index}, ' (', num2str(round(Metadata.Age(Index))) 'yo)'], 'FontWeight','normal', 'FontSize', 8)
     if IndexPlot>8*12
         IndexPlot = 1;
         chART.save_figure(['AllTopos_', num2str(Index), '.png'], ResultsFolder, PlotProps)
@@ -45,34 +43,34 @@ for Index =1:size(CleanTopo, 1)
 end
 
 
-%% Average Topography
+%% Figure 2: Average Topography & examples
 
 PlotProps = Parameters.PlotProps.Manuscript;
 PlotProps.Colorbar.Location = 'eastoutside';
 PlotProps.External.EEGLAB.TopoRes = 300;
 PlotProps.Axes.xPadding = 20;
 
-PlotTopos = {'NDAREF150LNT', 'NDARLE849LCR', 'NDARKE358TFU',  'NDARGT022BEW', 'NDARCW497XW2';
-   'NDAREV848HWX', 'NDARFJ803JF7', 'NDARJH441HJD', 'NDARFN452VPC', 'NDARFA737TG6'};
+PlotTopos = {'NDARZT199MF6', 'NDARFD453NPR', 'NDARXH140YZ0', 'NDARLL846UYQ', 'NDARPN886HH9';
+    'NDARKM635UY0', 'NDARAE710YWG', 'NDARTG681ZDV', 'NDARLZ986JLL', 'NDARVK847ZRT'};
 
 
 Grid = [2, size(PlotTopos, 2)+2];
 
 figure('Units','normalized', 'OuterPosition',[0 0 .5 .33])
 chART.sub_plot([], Grid, [2, 1], [2, 2], false, '', PlotProps);
-chART.plot.eeglab_topoplot(mean(CleanTopo, 1), Chanlocs, [], ...
+chART.plot.eeglab_topoplot(mean(CleanTopo, 1, 'omitnan'), Chanlocs, [], ...
     '', 'log power', 'Linear', PlotProps)
-title(['Average (N=', num2str(size(CleanTopo, 1)), ')'])
+title(['Average (N=', num2str(nnz(~isnan(CleanTopo(:, 1)))), ')'])
 
 PlotProps.Axes.xPadding = 15;
 for IndexR = 1:size(PlotTopos, 1)
     for IndexC = 1:size(PlotTopos, 2)
-        Index = find(strcmp(ClusterMetadata.EID, PlotTopos(IndexR, IndexC)));
+        Index = find(strcmp(Metadata.EID, PlotTopos(IndexR, IndexC)));
         Data = CleanTopo(Index, :);
         chART.sub_plot([], Grid, [IndexR, IndexC+2], [], false, '', PlotProps);
         chART.plot.eeglab_topoplot(Data, Chanlocs, [], quantile(Data, [.01, 1]), '', 'Linear', PlotProps)
 
-        title(ClusterMetadata.Participant(Index), 'FontWeight','normal', 'FontSize', PlotProps.Text.AxisSize)
+        title(Metadata.Participant(Index), 'FontWeight','normal', 'FontSize', PlotProps.Text.AxisSize)
     end
 end
 
