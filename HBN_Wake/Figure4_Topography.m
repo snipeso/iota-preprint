6a2e37b000caa82cae4c740c21c1fa9626829467
Sophia Snipes <snipeso@users.noreply.github.com>
2024-07-21 20:27:52 +0200

    update topography script

--- HBN_Wake/Figure4_Topography.m
+++ HBN_Wake/Figure4_Topography.m
@@ -14,10 +14,11 @@ if ~exist(ResultsFolder,'dir')
     mkdir(ResultsFolder)
 end
 
-load(fullfile(CacheDir, CacheName),  'Metadata', 'BandTopographies', 'Chanlocs')
+load(fullfile(CacheDir, CacheName),  'Metadata', 'CustomTopographies', 'Chanlocs')
 
-Remove = ~any(isnan(BandTopographies), 2);
-CleanTopo = BandTopographies(Remove, :);
+CustomTopographies = squeeze(CustomTopographies(:, end, :));
+Remove = ~any(isnan(CustomTopographies), 2);
+CleanTopo = CustomTopographies(Remove, :);
 ClusterMetadata = Metadata(~Remove, :);
 
 [~, Order] = sort(max(CleanTopo, [], 2), 'descend');
@@ -34,10 +35,10 @@ for Index =1:size(CleanTopo, 1)
     subplot(8, 12, IndexPlot)
     chART.plot.eeglab_topoplot(CleanTopo(Index, :), Chanlocs, [], [], '', 'Linear', PlotProps)
     IndexPlot = IndexPlot+1;
-    title(num2str(Index))
+    title([ClusterMetadata.EID{Index}, ' (', num2str(round(ClusterMetadata.Age(Index))) 'yo)'], 'FontWeight','normal', 'FontSize', 8)
     if IndexPlot>8*12
         IndexPlot = 1;
-        chART.save_figure(['AllTopos_', num2str(Index)], ResultsFolder, PlotProps)
+        chART.save_figure(['AllTopos_', num2str(Index), '.png'], ResultsFolder, PlotProps)
 
         figure('Units','normalized', 'OuterPosition',[0 0 1 1])
     end
@@ -50,10 +51,12 @@ PlotProps = Parameters.PlotProps.Manuscript;
 PlotProps.Colorbar.Location = 'eastoutside';
 PlotProps.External.EEGLAB.TopoRes = 300;
 PlotProps.Axes.xPadding = 20;
-PlotIndex = [ 330  34  472  453 214;
-    21  148 164  61 40];
 
-Grid = [2, size(PlotIndex, 2)+2];
+PlotTopos = {
+'NDARGN148MK2','NDARKG859AGN','NDAREV848HWX','NDARHW808VDT','NDARHX252NVH';
+'NDARKP815KPZ','NDARFN452VPC','NDARGY464EYL','NDAREF150LNT','NDARFR108JNB'};
+
+Grid = [2, size(PlotTopos, 2)+2];
 
 figure('Units','normalized', 'OuterPosition',[0 0 .5 .35])
 chART.sub_plot([], Grid, [2, 1], [2, 2], false, '', PlotProps);
@@ -62,9 +65,9 @@ chART.plot.eeglab_topoplot(mean(CleanTopo, 1), Chanlocs, [], ...
 title(['Average (N=', num2str(size(CleanTopo, 1)), ')'])
 
 PlotProps.Axes.xPadding = 15;
-for IndexR = 1:size(PlotIndex, 1)
-    for IndexC = 1:size(PlotIndex, 2)
-        Index = PlotIndex(IndexR, IndexC);
+for IndexR = 1:size(PlotTopos, 1)
+    for IndexC = 1:size(PlotTopos, 2)
+        Index = find(strcmp(ClusterMetadata.EID, PlotTopos(IndexR, IndexC)));
         Data = CleanTopo(Index, :);
         chART.sub_plot([], Grid, [IndexR, IndexC+2], [], false, '', PlotProps);
         chART.plot.eeglab_topoplot(Data, Chanlocs, [], quantile(Data, [.01, 1]), '', 'Linear', PlotProps)
@@ -77,5 +80,19 @@ for IndexR = 1:size(PlotIndex, 1)
 end
 % colormap(PlotProps.Color.Maps.Linear)
 
-        chART.save_figure(['AllTopos_', num2str(Index)], ResultsFolder, PlotProps)
+% chART.save_figure('AverageTopography', ResultsFolder, PlotProps)
+
+
+%% identify peak locations
+
+MeanTopo = mean(CleanTopo, 1);
+
+Topography = table();
+Topography.Labels = {Chanlocs.labels}';
+Topography.Iota = MeanTopo';
+
+disp(sortrows(Topography, 'Iota', 'descend'))
+
+
+
 
