e9ce414dbcf82c6130305d160383fe1f7096e741
Sophia Snipes <snipeso@users.noreply.github.com>
2024-07-25 10:11:08 +0200

    remove bad epochs and reref script sleep

---
+++ LSM_Sleep/Prep2_RemoveArtefacts.m
@@ -0,0 +1,84 @@
+% this script removes manually detected artefacts, re-references to the
+% average, and saves the data with the scoring
+
+clear
+clc
+close all
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Parameters
+
+P = LSMParameters();
+
+Paths  = P.Paths;
+Refresh = false;
+Task = P.Task;
+Format = 'Minimal'; % chooses which filtering to do
+RemovalThreshold = .1;
+
+ScoringIndexes = -3:1:1;
+ScoringLabels = {'N3', 'N2', 'N1', 'W', 'R'};
+
+% locations
+SourceScoring =  fullfile(Paths.Core, 'Outliers', Task);
+SourceEEG = fullfile(Paths.Preprocessed, Format, 'MAT', Task);
+Files = list_filenames(SourceEEG);
+
+Destination = fullfile(Paths.Preprocessed,  Format, 'Clean', Task);
+
+if ~exist(Destination, 'dir')
+    mkdir(Destination)
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% run
+
+for FileIdx = 1:numel(Files)
+
+    % check if destination file already exists
+    File = Files{FileIdx};
+    if ~Refresh && exist(fullfile(Destination, File), 'file')
+        disp(['Already did ', char(File)])
+        continue
+    end
+
+    % load in EEG
+    load(fullfile(SourceEEG, File), 'EEG')
+    EEG = eeg_checkset(EEG);
+    pop_eegplot(EEG)
+
+    % load in artefacts
+    Filename_Cuts = replace(File, '.mat', '_Cutting_artndxn.mat');
+
+    load(fullfile(SourceScoring, Filename_Cuts), 'artndxn', 'visnum', 'scoringlen')
+    Artefacts = ~artndxn; % artndxn indicates 1 to keep and 0 to remove
+    Scoring = visnum;
+
+
+    % reorder wake and REM, which was saved as 1 and 0, but should be
+    % switched
+    Scoring(Scoring==0) = 2;
+    Scoring(Scoring==1) = 0;
+    Scoring(Scoring==2) = 1;
+
+    % determine whether to remove channel or epoch
+    BadSegments = remove_channel_or_window(Artefacts, RemovalThreshold);
+
+    % for bad channels, remove and interpolate
+    BadChannels = all(BadSegments==1, 2);
+    EEG = pop_interp(EEG, find(BadChannels));
+
+    % average-reference data
+    EEG = pop_reref(EEG, []);
+
+    pop_eegplot(EEG)
+
+    % save
+    save(fullfile(Destination, File), 'EEG', 'BadSegments', 'Scoring', 'ScoringLabels', 'ScoringIndexes')
+    disp(['Finished ', File])
+end
+
+
+
