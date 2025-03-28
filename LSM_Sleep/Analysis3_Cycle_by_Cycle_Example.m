4c93faa88a785c8a2ab7da6a1a99ab0b171ab68a
Sophia Snipes <snipeso@users.noreply.github.com>
2025-03-28 16:52:06 +0100

    final but rough draft

--- LSM_Sleep/Analysis3_Cycle_by_Cycle_Example.m
+++ LSM_Sleep/Analysis3_Cycle_by_Cycle_Example.m
@@ -20,7 +20,7 @@ Channels = Parameters.Channels.NotEdge;
 ExampleParticipant = 'P09';
 
 load(fullfile(SourceSpecparam, [ExampleParticipant, '_Sleep_Baseline.mat']), ...
-    'PeriodicPeaks', 'Scoring', 'Chanlocs')
+    'PeriodicPeaks', 'Scoring', 'Chanlocs', 'Artefacts')
 
 load(fullfile(SourceEEG, [ExampleParticipant, '_Sleep_Baseline.mat']), 'EEG')
 
@@ -34,8 +34,9 @@ BandRange = MaxPeak(1) + [-2 2];
 %%
 
 % select only REM sleep
-[Starts, Ends] = data2windows(Scoring==1);
+[Starts, Ends] = data2windows(Scoring==1 & ~all(Artefacts));
 REMWindows = [Starts', Ends']*20;
+REMWindows = REMWindows(4, :);
 EEGSnippet = pop_select(EEG, 'time', REMWindows);
 
 % filter to remove drifts
@@ -45,9 +46,10 @@ EEGSnippet = pop_eegfiltnew(EEGSnippet, [], 45);
 
 %%
 Bursts = burst_detection(EEGSnippet, BandRange, CriteriaSet);
-
+Bursts = cycy.average_cycles(Bursts, {'Amplitude'});
+BurstClusters = cycy.aggregate_bursts_into_clusters(Bursts, EEGSnippet, 1);
 CacheDir = Paths.Cache;
-CacheName = [ExampleParticipant, '_Bursts.mat'];
+CacheName = [ExampleParticipant, '_BurstsOneREM.mat'];
 
 save(fullfile(CacheDir, CacheName), 'Bursts', 'EEGSnippet', 'BurstClusters')
 
@@ -60,7 +62,6 @@ load(fullfile(CacheDir, CacheName), 'Bursts', 'EEGSnippet', 'BurstClusters')
 %%  
 
 
-BurstClusters = cycy.aggregate_bursts_into_clusters(Bursts, EEGSnippet, 1);
 
 %%
 Density = nan(1, numel(Chanlocs));
@@ -75,3 +76,18 @@ PlotProps.Colorbar.Location = 'eastoutside';
 figure
 chART.plot.eeglab_topoplot(Density, Chanlocs, [], [0 50], 'bursts/min', 'Linear', PlotProps)
 
+
+cycy.plot.plot_all_bursts(EEGSnippet, 20, Bursts, 'Band');
+
+%%
+EOG2 =  EEGSnippet.data(32, :)-EEGSnippet.data(125, :);
+
+rms_per_channel = sqrt(EOG2.^2);
+t = linspace(0, size(EEGSnippet.data, 2)/EEGSnippet.srate, size(EEGSnippet.data, 2));
+
+figure('Units','centimeters','Position',[0 0 25 8])
+
+scatter([Bursts.Start]/EEG.srate/60, [Bursts.ChannelIndex], [Bursts.MeanAmplitude]*5, 'filled', 'MarkerFaceAlpha', .1, 'MarkerFaceColor', PlotProps.Color.Maps.Linear(128, :))
+hold on
+plot(t/60, mat2gray(smooth(rms_per_channel, 1000))*129, 'LineWidth', 2, 'color', [0 0 0])
+axis tight
